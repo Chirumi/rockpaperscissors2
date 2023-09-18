b93a7d859dc57e0884027529b931af06b532adc5
michiru <kirittoes@gmail.com>
2023-09-18 16:46:56 +0100

    Add text for displaying weapon choices and results

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let computerScore = 0
 const rock = document.querySelector(".rock")
 const paper = document.querySelector(".paper")
 const scissors = document.querySelector(".scissors")
+const playerChoice = document.querySelector(".playerChoice")
+const computerChoice = document.querySelector(".computerChoice")
+const result = document.querySelector(".result")
 
 rock.addEventListener("click", () => {playRound("rock")})
 paper.addEventListener("click", () => {playRound("paper")})
@@ -28,55 +31,55 @@ function playRound(weaponChoice) { // Plays a round of rock, paper, scissors
     let computerSelection = getComputerChoice()
 
     if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("You've picked rock!")
-        console.log("Computer picked rock!")
-        console.log("Tied round!")
+        playerChoice.innerHTML = "You chose: ROCK"
+        computerChoice.innerHTML = "Computer chose: ROCK"
+        result.innerHTML = "Result: TIED!"
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You've picked rock!")
-        console.log("Computer picked paper!")
+        playerChoice.innerHTML = "You chose: ROCK"
+        computerChoice.innerHTML = "Computer chose: PAPER"
         computerScore++
-        console.log("You lost the round!")
+        result.innerHTML = "Result: YOU LOST!"
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You've picked rock!")
-        console.log("Computer picked scissors!")
+        playerChoice.innerHTML = "You chose: ROCK"
+        computerChoice.innerHTML = "Computer chose: SCISSORS"
         playerScore++
-        console.log("You won the round!")
+        result.innerHTML = "Result: YOU WON!"
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You've picked paper!")
-        console.log("Computer picked rock!")
+        playerChoice.innerHTML = "You chose: PAPER"
+        computerChoice.innerHTML = "Computer chose: ROCK"
         playerScore++
-        console.log("You won the round!")
+        result.innerHTML = "Result: YOU WON!"
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("You've picked paper!")
-        console.log("Computer picked paper!")
-        console.log("Tied round!")
+        playerChoice.innerHTML = "You chose: PAPER"
+        computerChoice.innerHTML = "Computer chose: PAPER"
+        result.innerHTML = "Result: TIED!"
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You've picked paper!")
-        console.log("Computer picked scissors!")
+        playerChoice.innerHTML = "You chose: PAPER"
+        computerChoice.innerHTML = "Computer chose: SCISSORS"
         computerScore++
-        console.log("You lost the round!")
+        result.innerHTML = "Result: YOU LOST!"
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You've picked scissors!")
-        console.log("Computer picked rock!")
+        playerChoice.innerHTML = "You chose: SCISSORS"
+        computerChoice.innerHTML = "Computer chose: ROCK"
         computerScore++
-        console.log("You lost the round!")
+        result.innerHTML = "Result: YOU LOST!"
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You've picked scissors!")
-        console.log("Computer picked paper!")
+        playerChoice.innerHTML = "You chose: SCISSORS"
+        computerChoice.innerHTML = "Computer chose: PAPER"
         playerScore++
-        console.log("You won the round!")
+        result.innerHTML = "Result: YOU WON!"
     }
     else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        console.log("You've picked scissors!")
-        console.log("Computer picked scissors!")
-        console.log("Tied round!")
+        playerChoice.innerHTML = "You chose: SCISSORS"
+        computerChoice.innerHTML = "Computer chose: SCISSORS"
+        result.innerHTML = "Result: TIED!"
     }
     else {
         console.log("Choose between Rock, Paper or Scissors!")
