806def6bd57af5b028e04646d6e46e545a43ee74
michiru <kirittoes@gmail.com>
2023-09-18 17:57:13 +0100

    Add a retry button and disallow gameplay

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Make retry button that only appears if playerScore OR computerScore reaches 5 when game ends
+
 let playerScore = 0
 let computerScore = 0
+let retry = 0 // If value is 0, retry button will appear. If value is 1, retry button will not appear
 
 const rock = document.querySelector(".rock")
 const paper = document.querySelector(".paper")
@@ -9,97 +12,110 @@ const computerChoice = document.querySelector(".computerChoice")
 const result = document.querySelector(".result")
 const pScore = document.querySelector(".pScore")
 const cScore = document.querySelector(".cScore")
+const gameResult = document.querySelector(".gameResult")
+const scores = document.querySelector(".scores")
 
 rock.addEventListener("click", () => {playRound("rock")})
 paper.addEventListener("click", () => {playRound("paper")})
 scissors.addEventListener("click", () => {playRound("scissors")})
 
 function playRound(weaponChoice) { // Plays a round of rock, paper, scissors
-    let playerSelection = weaponChoice
+    if ((!(playerScore == 5 || computerScore == 5)) && retry == 0) {
+        let playerSelection = weaponChoice
 
-    function getComputerChoice() { // Computer's choices of rock, paper or sicssors
-        let randomNumber = Math.floor(Math.random()*3)
-        if (randomNumber === 0) {
-            return "rock"
+        function getComputerChoice() { // Computer's choices of rock, paper or sicssors
+            let randomNumber = Math.floor(Math.random()*3)
+            if (randomNumber === 0) {
+                return "rock"
+            }
+            else if (randomNumber === 1) {
+                return "paper"
+            } 
+            else {
+                return "scissors"
+            }
         }
-        else if (randomNumber === 1) {
-            return "paper"
-        } 
+
+        let computerSelection = getComputerChoice()
+
+        if (playerSelection === "rock" && computerSelection === "rock") {
+            playerChoice.innerHTML = "You chose: ROCK"
+            computerChoice.innerHTML = "Computer chose: ROCK"
+            result.innerHTML = "Result: TIED ROUND!"
+        }
+        else if (playerSelection === "rock" && computerSelection === "paper") {
+            playerChoice.innerHTML = "You chose: ROCK"
+            computerChoice.innerHTML = "Computer chose: PAPER"
+            computerScore++
+            result.innerHTML = "Result: YOU LOST THE ROUND!"
+        }
+        else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerChoice.innerHTML = "You chose: ROCK"
+            computerChoice.innerHTML = "Computer chose: SCISSORS"
+            playerScore++
+            result.innerHTML = "Result: YOU WON THE ROUND!"
+        }
+        else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerChoice.innerHTML = "You chose: PAPER"
+            computerChoice.innerHTML = "Computer chose: ROCK"
+            playerScore++
+            result.innerHTML = "Result: YOU WON THE ROUND!"
+        }
+        else if (playerSelection === "paper" && computerSelection === "paper") {
+            playerChoice.innerHTML = "You chose: PAPER"
+            computerChoice.innerHTML = "Computer chose: PAPER"
+            result.innerHTML = "Result: TIED ROUND!"
+        }
+        else if (playerSelection === "paper" && computerSelection === "scissors") {
+            playerChoice.innerHTML = "You chose: PAPER"
+            computerChoice.innerHTML = "Computer chose: SCISSORS"
+            computerScore++
+            result.innerHTML = "Result: YOU LOST THE ROUND!"
+        }
+        else if (playerSelection === "scissors" && computerSelection === "rock") {
+            playerChoice.innerHTML = "You chose: SCISSORS"
+            computerChoice.innerHTML = "Computer chose: ROCK"
+            computerScore++
+            result.innerHTML = "Result: YOU LOST THE ROUND!"
+        }
+        else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerChoice.innerHTML = "You chose: SCISSORS"
+            computerChoice.innerHTML = "Computer chose: PAPER"
+            playerScore++
+            result.innerHTML = "Result: YOU WON THE ROUND!"
+        }
+        else if (playerSelection === "scissors" && computerSelection === "scissors") {
+            playerChoice.innerHTML = "You chose: SCISSORS"
+            computerChoice.innerHTML = "Computer chose: SCISSORS"
+            result.innerHTML = "Result: TIED ROUND!"
+        }
         else {
-            return "scissors"
+            console.log("Choose between Rock, Paper or Scissors!")
         }
-    }
 
-    let computerSelection = getComputerChoice()
-
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        playerChoice.innerHTML = "You chose: ROCK"
-        computerChoice.innerHTML = "Computer chose: ROCK"
-        result.innerHTML = "Result: TIED!"
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        playerChoice.innerHTML = "You chose: ROCK"
-        computerChoice.innerHTML = "Computer chose: PAPER"
-        computerScore++
-        result.innerHTML = "Result: YOU LOST!"
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerChoice.innerHTML = "You chose: ROCK"
-        computerChoice.innerHTML = "Computer chose: SCISSORS"
-        playerScore++
-        result.innerHTML = "Result: YOU WON!"
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerChoice.innerHTML = "You chose: PAPER"
-        computerChoice.innerHTML = "Computer chose: ROCK"
-        playerScore++
-        result.innerHTML = "Result: YOU WON!"
-    }
-    else if (playerSelection === "paper" && computerSelection === "paper") {
-        playerChoice.innerHTML = "You chose: PAPER"
-        computerChoice.innerHTML = "Computer chose: PAPER"
-        result.innerHTML = "Result: TIED!"
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        playerChoice.innerHTML = "You chose: PAPER"
-        computerChoice.innerHTML = "Computer chose: SCISSORS"
-        computerScore++
-        result.innerHTML = "Result: YOU LOST!"
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        playerChoice.innerHTML = "You chose: SCISSORS"
-        computerChoice.innerHTML = "Computer chose: ROCK"
-        computerScore++
-        result.innerHTML = "Result: YOU LOST!"
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerChoice.innerHTML = "You chose: SCISSORS"
-        computerChoice.innerHTML = "Computer chose: PAPER"
-        playerScore++
-        result.innerHTML = "Result: YOU WON!"
-    }
-    else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        playerChoice.innerHTML = "You chose: SCISSORS"
-        computerChoice.innerHTML = "Computer chose: SCISSORS"
-        result.innerHTML = "Result: TIED!"
+        pScore.innerHTML = `Your score: ${playerScore}`
+        cScore.innerHTML = `Your score: ${computerScore}`
+        winner()
     }
     else {
-        console.log("Choose between Rock, Paper or Scissors!")
+        if ((playerScore == 5 || computerScore == 5) && retry == 0) {
+            let retryButton = document.createElement("button")
+            retryButton.innerHTML = "RETRY"
+            retryButton.addEventListener("click", () => {
+                
+            })
+            scores.appendChild(retryButton)
+            retry = 1
+        }
     }
-
-    pScore.innerHTML = `Your score: ${playerScore}`
-    cScore.innerHTML = `Your score: ${computerScore}`
 }
 
 function winner() {
-    if (playerScore < computerScore) {
-        console.log("YOU'VE LOST THE GAME!")
+    if (computerScore == 5) {
+        gameResult.innerHTML = "YOU LOST THE GAME!"
     } 
-    else if (playerScore > computerScore) {
-        console.log("YOU'VE WON THE GAME!")
-    }
-    else {
-        console.log("THE GAME IS TIED!")
+    else if (playerScore == 5) {
+        gameResult.innerHTML = "YOU WON THE GAME!"
     }
 }
 
